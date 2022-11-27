b6c69e8c88ecd803bea9f1472d56fcf2df12317d
Keskil Nikolaev <nik96keskil@yandex.ru>
2022-11-28 04:54:53 +0700

    PR14 notFound fix3

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
-const notFoundControllers = require('../controllers/notFoundControllers');
+const { notFoundControllers } = require('../controllers/notFoundControllers');
 const { loginCelebrate, createUserCelebrate } = require('../utils/celebrate');
 const { login, createUser } = require('../controllers/users');
 const auth = require('../middlewares/auth');
